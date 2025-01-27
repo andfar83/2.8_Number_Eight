344257468225ab03fe542eafc3710b169853da94
andfar83 <andresavila@students.abtech.edu>
2025-01-26 22:42:21 -0500

    Add files via upload

---
+++ number_8.py
@@ -0,0 +1,11 @@
+# Addition operation resulting in 8
+print("Addition: 5 + 3 =", 5 + 3)
+
+# Subtraction operation resulting in 8
+print("Subtraction: 16 - 8 =", 16 - 8)
+
+# Multiplication operation resulting in 8
+print("Multiplication: 4 * 2 =", 4 * 2)
+
+# Division operation resulting in 8
+print("Division: 64 / 8 =", 64 / 8)
